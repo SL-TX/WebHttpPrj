6149518b7edf4a3bb082275d05cfdcdb53cfc3a2
sl-tx <sl-tx@yandex.ru>
2023-01-22 14:51:39 +0300

    init

---
+++ src/main/java/ru/skypro/webhttpprj/WebHttpPrjApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.webhttpprj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebHttpPrjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebHttpPrjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/skypro/webhttpprj/WebHttpPrjApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.webhttpprj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebHttpPrjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
